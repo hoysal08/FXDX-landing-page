924123176eac588a4e62960dd9541fdd0e22e882
Abhijit Kumar <12881565+abhijitkrm@users.noreply.github.com>
2022-08-20 12:34:11 +0530

    Update Investors.js

--- src/components/Investors.js
+++ src/components/Investors.js
@@ -4,13 +4,15 @@ import InvestorsImg from "./InvestorsImg";
 
 const Investors = () => {
     const investorsImg = [
+        { img: "./bc.png" },
+        { img: "./algo.png" },
+        { img: "./borderless.png" },
+        { img: "./ripple.png" },
         { img: "./ex.png" },
         { img: "./valhalla.png" },
-        { img: "./bc.png" },
         { img: "./crown.png" },
         { img: "./ventures.png" },
-        { img: "./gsr.png" },
-        { img: "./algo.png" },
+        { img: "./gsr.png" },        
         { img: "./polygon.png" },
         { img: "./dfyn.png" },
         { img: "./s.png" },
@@ -18,8 +20,7 @@ const Investors = () => {
         { img: "./meta.png" },
         { img: "./sheesha.png" },
         { img: "./contango.png" },
-        { img: "./hodl.png" },
-        { img: "./ripple.png" },
+        { img: "./hodl.png" }
         { img: "./shima.png" },
         { img: "./kretos.png" },
         { img: "./x.png" },
